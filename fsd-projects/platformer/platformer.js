7491d767cf231394e9fc11b1c4540f5772a00ef5
Imari Dupree <145306697+imaridupree125@users.noreply.github.com>
2025-09-17 18:19:10 +0000

    First Website

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,32 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+      createPlatform(125, 620, 100, 170, "black")
+      createPlatform(375, 575, 100, 200, "hotpink")
+      createPlatform(490, 575, 75, 20, "black")
+      createPlatform(600, 520, 150, 1000, "black")
+      createPlatform(400, 390, 100, 20, "black")
+      createPlatform(200, 290, 100, 20, "hotpink")
+      createPlatform(100, 175, 100, 20, "black")
+      createPlatform(300, 175, 700, 20, "hotpink")
+      createPlatform(1050, 200, 100, 20, "black")
+      
     // TODO 3 - Create Collectables
-
+      createCollectable("steve", 410, 375);
+      createCollectable("diamond", 490, 550);
+      createCollectable("grace", 1100, 175)
 
 
     
     // TODO 4 - Create Cannons
-
+      createCannon("top", 350, 2000);
+      createCannon("right", 200, 2000);
+      createCannon("left", 360, 2000)
+      createCannon("bottom", 1150, 2000)
 
     
     
